fd66c9f944a47e923a5a5b4e5affb6ac666e600e
Altoria-276 <2760579762@qq.com>
2024-11-22 21:37:05 +0800

    ui

--- ui.py
+++ ui.py
@@ -1,4 +1,7 @@
+from random import choice, choices
+from turtle import update
 import gradio as gr
+import pandas as pd
 
 
 def ui():
@@ -7,9 +10,14 @@ def ui():
         with gr.Tab("影响分析"):
             gr.Markdown("# 现存污染源贡献量变化影响分析")
             with gr.Row(equal_height=True):
-                gr.File(label="文件路径", scale=3)
-                gr.Dropdown(choices=["A", "B", "C"], label="选择拟合模型", scale=3)
-                gr.Button(value="运行", scale=1)
+                file_path_input = gr.File(label="文件路径", scale=3)
+                sheet_name_input = gr.Dropdown(choices=["无"], value="无", label="选择数据页", interactive=True)
+                model_input = gr.Dropdown(choices=["Ridge", "Linear", "C"], label="选择拟合模型", scale=3)
+
+            with gr.Row(equal_height=True):
+                feather_input = gr.Dropdown(choices=[], label="自变量特征选择", multiselect=True, interactive=True)
+                target_input = gr.Dropdown(choices=[], label="因变量特征选择", interactive=True)
+                run_button = gr.Button(value="运行", scale=1)
             with gr.Row(equal_height=True):
                 gr.Image(label="结果因果模型")
 
@@ -59,6 +67,27 @@ def ui():
                         gr.Button("计算")
                     with gr.Row(equal_height=True):
                         gr.Image(label="未来超标风险区空间分布图")
+
+        file_path_input.change(
+            fn=lambda file_path: gr.update(choices=pd.ExcelFile(file_path).sheet_names),
+            inputs=file_path_input,
+            outputs=sheet_name_input,
+        )
+
+        def sheet_change(file_path: str, sheet_name: str):
+            data = pd.ExcelFile(file_path).parse(sheet_name)
+            update = gr.update(choices=data.columns.to_list())
+            return update, update
+
+        sheet_name_input.change(fn=sheet_change, inputs=[file_path_input, sheet_name_input], outputs=[target_input, feather_input])
+
+        # target_input.change(
+        #     fn=lambda file_path, sheet_name, target_name: gr.update(
+        #         choices=[col for col in pd.ExcelFile(file_path).parse(sheet_name).columns.to_string() if col != target_name]
+        #     ),
+        #     inputs=[file_path_input, sheet_name_input, target_input],
+        #     outputs=feather_input,
+        # )
 
     ui.launch(share=False)
 
